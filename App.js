d538f69f8d220448ee0b5c0fd7ba73d33614172e
Soireee <soireele@gmail.com>
2020-09-30 01:46:59 +0900

    add body

--- App.js
+++ App.js
@@ -1,22 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, TouchableOpacity, Text, View } from 'react-native';
-import logo from './assets/splash.png';
-import BodyText from './src/elements/BodyText';
+import { StyleSheet, Text, View } from 'react-native';
+// import BodyText from './src/elements/BodyText';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color:'#267c7c', fontSize: 18}}>HelloWorld</Text>
-      <StatusBar style="auto" />
-      <Image source={logo} style={{ width: 305, height: 159 }} /> 
-      <TouchableOpacity
-        onPress={() => alert('Hello, world!')}
-        style={{ backgroundColor: 'blue' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
-      </TouchableOpacity>
-      {/* BodyTextにHiを送ると、そこにstyleが修正される。コレはBodyText,js側でthis.props.childrenを指定しているから */}
-      <BodyText>Hi</BodyText>
+      <View style={styles.appbar}>
+        <View>
+          <Text style={styles.appbarTitle}>Memo</Text>
+        </View>
+      </View>
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2020/09/09</Text>
+        </View>
+
+      </View>
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2020/09/09</Text>
+        </View>
+
+      </View>
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2020/09/09</Text>
+        </View>
+
+      </View>
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2020/09/09</Text>
+        </View>
+
+      </View>
+      <View style={styles.memoAddButton}>
+        <Text style={styles.memoAddText}>+</Text>
+      </View>
     </View>
   );
 }
@@ -24,8 +49,63 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffdf6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 70,
+  },
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 70,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 3.9,
+    zIndex: 10,
+  },
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  }, 
+  memoDate: {
+    fontSize: 12,
+    color: '#2a2a2a',
+  }, 
+  memoList: {
+    backgroundColor: '#fff',
+    width: '100%',
+    flex: 1,
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#fff',
+  },
+  memoAddButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 38,
+    height: 38,
+    backgroundColor: '#F31678',
+    borderRadius: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  memoAddText: {
+    position: 'absolute',
+    lineHeight: 24,
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
